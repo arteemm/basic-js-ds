bac582c1d4bd978bf576a0cd58f22d863698ac99
arteemm <arteemmarteemm@gmail.com>
2021-10-18 20:48:52 +0300

    feat: add some solutions

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,40 +7,134 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 module.exports = class BinarySearchTree {
-  
+  constructor () {
+    this.head = null;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   return this.head;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add( data ) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let currentNode = this.head;
+
+    while(currentNode) {
+      if (newNode.data < currentNode.data) {
+        if(!currentNode.left) {
+          currentNode.left = newNode;
+          return;
+        }
+
+        currentNode = currentNode.left;
+      } else {
+        if(!currentNode.right) {
+          currentNode.right = newNode;
+          return;
+        }
+
+        currentNode = currentNode.right;
+      }
+    }
+  }
+
+  preOrder(node, callback) {
+    if(!node) {
+      return;
+    }
+    if(callback) {
+      callback(node);
+    }
+    this.preOrder(node.left, callback);
+    this.preOrder(node.right, callback);
+  }
+
+  traverseDFS(callback) {
+    
+     return this.preOrder(this.head, callback);
+    
+  }  
+
+  has(data) {
+    let b = false;
+    let g = this.traverseDFS((node) => {
+      if (node.data === data) {
+       return b = true;
+      } 
+    });
+    return b;
+  }
+
+  find(data) {
+    let b = null;
+    let g = this.traverseDFS((node) => {
+      if (node.data === data) {
+       return b = data;
+      } 
+    });
+    return b;
+  }
+
+  minNode(node) {
+    if (node.left === null)
+        return node;
+    else
+        return this.min();
 }
-
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.head = this.removeNode(this.head, data);
   }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+removeNode(node, data) {
+    if (node === null) {
+        return null;
+    } else if (data < node.data) {
+        node.left = this.removeNode(node.left, data);
+        return node;
+    } else if (data > node.data) {
+        node.right = this.removeNode(node.right, data);
+        return node;
+    } else {
+        if (node.left === null && node.right === null) {
+            node = null;
+            return node;
+        }
+        if (node.left === null) {
+            node = node.right;
+            return node;
+        } else if(node.right === null) {
+            node = node.left;
+            return node;
+        }
+        let newNode = this.minNode(node.right);
+        node.data = newNode.data;
+        node.right = this.removeNode(node.right, newNode.data);
+        return node;
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let b = this.head.data;
+    let g = this.traverseDFS((node) => {
+      if (node.data < b) {
+        b = node.data;
+      } 
+    });
+    return b;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let b = this.head.data;
+    let g = this.traverseDFS((node) => {
+      if (node.data > b) {
+        b = node.data;
+      } 
+    });
+    return b;
+
   }
 
 }
